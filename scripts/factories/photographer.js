2677245a7dc542da277bfc932f6665aad8152c50
Valentin Pierre <valentin.pierre71@gmail.com>
2022-01-24 16:37:09 +0100

    Lien entre la page index et la page photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,18 +6,14 @@ function photographerFactory(data) {
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         article.innerHTML = `
-            <img src="${picture}"/>
-            <h2>${name}</h2>
-            <p class="location">${city}, ${country}</p>
-            <p class="tagline">${tagline}</p>
-            <p class="price">${price}€/jour</p>
+            <a href="photographer.html">
+                <img src="${picture}"/>
+                <h2>${name}</h2>
+                <p class="location">${city}, ${country}</p>
+                <p class="tagline">${tagline}</p>
+                <p class="price">${price}€/jour</p>
+            </a>
         `
-        /*const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);*/
         return (article);
     }
     return { name, picture, city, country, tagline, price, getUserCardDOM }
