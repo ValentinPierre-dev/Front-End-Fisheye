8aeb6c06a62417efa53ec3a82cab9c7c68d72df7
Valentin Pierre <valentin.pierre71@gmail.com>
2022-01-24 11:24:38 +0100

    Modification HTML css

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,13 @@
 function displayModal() {
+    const background = document.getElementById("modal_bg");
+	background.style.display = "block";
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
 }
 
 function closeModal() {
+    const background = document.getElementById("modal_bg");
+    background.style.display = "none";
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
