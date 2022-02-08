e82fca64040f8281bd4bd0d6ca0706132318f620
Valentin Pierre <valentin.pierre71@gmail.com>
2022-02-08 14:41:07 +0100

    Ajout de la lightbox, des coeurs incrémentables et de l'incrémentation du total des likes

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,55 +1,28 @@
-// Open the Lightbox
-function openLightbox() {
-    document.getElementById("myLightbox").style.display = "block";
-}
+// Factory dédiée à la ligntbox
 
-// Close the Lightbox
-function closeLightbox() {
-    document.getElementById("myLightbox").style.display = "none";
-}
+function lightboxFactory(medias, currentId) {
 
-var slideIndex = 1;
-showSlides(slideIndex);
+    let index = medias.findIndex(m => m.id === currentId);
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+    function displayLightbox() {
+        
+        const dom = document.createElement('div');
+        dom.classList.add('lightbox');
+        dom.innerHTML = `
+            <button class="lightbox__close" onclick="closeLightbox()">Fermer</button>
+            <button class="lightbox__next">Suivant</button>
+            <button class="lightbox__prev">Précédent</button>
+            <div class="lightbox__container">
+                <img src="assets/photos/Photographers ID photos/${medias[index].image}" alt="">
+            </div>
+        `
+        const body = document.querySelector('body')
+        body.appendChild(dom)
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        return (dom);
     }
 
-    slides[slideIndex-1].style.display = "block";
-    console.log(slides[0]);
-    
+
+
+    return { displayLightbox }
 }
-
-window.addEventListener("keydown", function(e){
-    if (e.defaultPrevented) {
-        return; // Ne devrait rien faire si l'événement de la touche était déjà consommé.
-}
-
-    switch (e.key) {
-
-        case "ArrowLeft": plusSlides(-1)
-        break;
-
-        case "ArrowRight": plusSlides(1)
-        break;
-        default: return; // Quitter lorsque cela ne gère pas l'événement touche.
-    }
-    
-// Annuler l'action par défaut pour éviter qu'elle ne soit traitée deux fois.
-    e.preventDefault();
-}, true);
